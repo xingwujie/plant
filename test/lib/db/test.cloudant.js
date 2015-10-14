a03f3702349da0defe280919aacd89d1e5b6d642
Guy Ellis <wildfiction@gmail.com>
2015-10-14 09:27:25 -0700

    Add user create and cloudant DB

---
+++ test/lib/db/test.cloudant.js
@@ -0,0 +1,21 @@
+import {db} from '../../../lib/db';
+
+describe('/db/cloudant/', function() {
+  this.timeout(5000);
+  
+  it('should create a user account', (done) => {
+
+    const user = {
+      email: 'test@test.com',
+      first: 'first',
+      last: 'last'
+    };
+
+    db.createUser(user, (err, body) => {
+      // TODO: Add some asserts here.
+      console.log(body);
+      done();
+    });
+  });
+
+});
