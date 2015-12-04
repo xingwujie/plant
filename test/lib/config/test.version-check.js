45aa78fe997d98fac6539cdc85f538ad78f9abaf
Guy Ellis <wildfiction@gmail.com>
2015-12-04 08:49:36 -0700

    Change how version checking is done. Upgrade to 5.1.1

--- test/lib/config/test.version-check.js
+++ test/lib/config/test.version-check.js
@@ -1,11 +1,10 @@
-import assert from 'assert';
-import versionCheck from '../../../lib/config/version-check';
 
 describe('/version-check/', function() {
 
-  it('should check that we are testing with the right version of node', () => {
+  it('should check that we are testing with the right version of node', (done) => {
 
-    assert(versionCheck());
+    require('nodeversioncheck');
+    done();
 
   });
 
