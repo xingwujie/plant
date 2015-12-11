7afab3f635cb69b6e4f04f269e13aec3a60fe524
Guy Ellis <wildfiction@gmail.com>
2015-12-10 21:45:36 -0700

    Added node-version-checker. Thanks to the amazing @ellisande

--- test/lib/config/test.version-check.js
+++ test/lib/config/test.version-check.js
@@ -3,7 +3,7 @@ describe('/version-check/', function() {
 
   it('should check that we are testing with the right version of node', (done) => {
 
-    require('nodeversioncheck');
+    require('node-version-checker');
     done();
 
   });
