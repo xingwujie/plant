91d1b48a63badd12a1778dd1bc592b508b3f8e73
Guy Ellis <wildfiction@gmail.com>
2015-12-12 10:38:30 -0700

    Changed plant classification fields. Cultivar > Common & Botanical names.

--- app/components/plant/AddPlant.jsx
+++ app/components/plant/AddPlant.jsx
@@ -18,7 +18,7 @@ export default AuthRequired(class AddPlant extends React.Component {
   save(e) {
     if(this.state.title) {
       var plant = _.pick(this.state,
-        ['title', 'cultivar', 'description', 'purchasedDate', 'plantedDate', 'price']
+        ['title', 'botanicalName', 'commonName', 'description', 'purchasedDate', 'plantedDate', 'price']
       );
       PlantActions.create(plant, (err, savedPlant) => {
         console.log('PlantActions.create cb:', err, savedPlant);
@@ -57,7 +57,8 @@ export default AuthRequired(class AddPlant extends React.Component {
   render() {
     var {
       title,
-      cultivar,
+      botanicalName,
+      commonName,
       description,
       purchasedDate,
       plantedDate,
@@ -69,23 +70,35 @@ export default AuthRequired(class AddPlant extends React.Component {
         <h2 style={{textAlign: 'center'}}>Add Plant</h2>
           <form className='editor'>
 
-            <div className='form-group title-input-combo col-xs-12 col-sm-6'>
+            <div className='form-group title-input-combo col-xs-12'>
               <label>Title:</label>
               <input autoFocus className='form-control'
                 type='text' value={title}
-                placeholder='How do you refer to this plant?'
+                placeholder='How do you refer to this plant? (e.g. Washington Navel)'
                 onChange={this.handleChange.bind(this, 'title')} />
             </div>
 
-            <div className='form-group title-input-combo col-xs-12 col-sm-6'>
-              <label>Cultivar:</label>
-              <input className='form-control'
-                type='text' value={cultivar}
-                placeholder='Cultivar or variety or general name'
-                onChange={this.handleChange.bind(this, 'cultivar')} />
+            <div className='col-xs-12'>
+              {'The rest of the fields are optional. You can come back and add them later if you want to start adding notes or other plants now.'}
             </div>
 
-            <div className='form-group title-input-combo'>
+            <div className='form-group title-input-combo col-xs-12 col-sm-6'>
+              <label>Botanical Name:</label>
+              <input className='form-control'
+                type='text' value={botanicalName}
+                placeholder={'e.g. Citrus sinensis \'Washington Navel\''}
+                onChange={this.handleChange.bind(this, 'botanicalName')} />
+            </div>
+
+            <div className='form-group title-input-combo col-xs-12 col-sm-6'>
+              <label>Common Name:</label>
+              <input className='form-control'
+                type='text' value={commonName}
+                placeholder='e.g. Washington Navel Orange'
+                onChange={this.handleChange.bind(this, 'commonName')} />
+            </div>
+
+            <div className='form-group title-input-combo col-xs-12'>
               <label>Description:</label>
               <textarea className='form-control'
                 rows='2' value={description}
@@ -117,7 +130,7 @@ export default AuthRequired(class AddPlant extends React.Component {
                 onChange={this.handleChange.bind(this, 'price')} />
             </div>
 
-            <div style={{textAlign: 'center'}}>
+            <div className='form-group col-xs-12' style={{textAlign: 'center'}}>
               <button style={{fontSize: 'xx-large'}} className='btn btn-primary' type='button' onClick={this.save.bind(this)}>Save</button>
             </div>
 
