a12df9139c5f1764692c79a8685ffa9065c66cf4
Guy Ellis <wildfiction@gmail.com>
2015-12-29 19:49:35 -0700

    Add reducer tests

---
+++ test/app/reducers/test.reducers.js
@@ -0,0 +1,19 @@
+import rootReducer from '../../../app/reducers';
+import * as actions from '../../../app/actions';
+import assert from 'assert';
+
+describe('/app/reducers', function() {
+
+  it('should reduce a logout action', (done) => {
+    const expected = {
+      plants: [],
+      user: {
+        plants: []
+      }
+    };
+    const actual = rootReducer({}, actions.logout());
+    assert.deepEqual(actual, expected);
+    done();
+  });
+
+});
---
+++ test/app/reducers/test.user.js
@@ -0,0 +1,51 @@
+import user from '../../../app/reducers/user';
+import * as actions from '../../../app/actions';
+import assert from 'assert';
+
+describe('/app/reducers/user', function() {
+
+  it('should reduce a logout action', (done) => {
+    const expected = {
+      plants: []
+    };
+    const actual = user({}, actions.logout());
+    assert.deepEqual(actual, expected);
+    done();
+  });
+
+  it('should reduce a login request', (done) => {
+    const payload = {one: 1, two: 2};
+    const expected = {
+      plants: [],
+      status: 'fetching'
+    };
+    const actual = user({}, actions.loginRequest(payload));
+    assert.deepEqual(actual, expected);
+    done();
+  });
+
+  it('should reduce a login success', (done) => {
+    const payload = {one: 1, two: 2};
+    const expected = Object.assign({
+      plants: [],
+      status: 'success',
+      isLoggedIn: true
+    }, payload);
+    const actual = user({}, actions.loginSuccess(payload));
+    assert.deepEqual(actual, expected);
+    done();
+  });
+
+  it('should reduce a login failure', (done) => {
+    const payload = {one: 1, two: 2};
+    const expected = Object.assign({
+      plants: [],
+      status: 'failed',
+      isLoggedIn: false
+    }, payload);
+    const actual = user({}, actions.loginFailure(payload));
+    assert.deepEqual(actual, expected);
+    done();
+  });
+
+});
