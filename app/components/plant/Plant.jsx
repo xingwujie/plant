092c951578c59b2c63afecbe783203639cb7c3d3
Guy Ellis <wildfiction@gmail.com>
2016-01-19 06:33:00 -0700

    If user logs out then switch to read mode

--- app/components/plant/Plant.jsx
+++ app/components/plant/Plant.jsx
@@ -53,8 +53,17 @@ export default class Plant extends React.Component {
       };
     }
     // console.log('initState plant:', plant, _id, props);
+    const owner = plant && isOwner(plant, user);
+
+    if(!owner && plant.mode !== 'read') {
+      store.dispatch(actions.setPlantMode({
+        _id: plant._id,
+        mode: 'read'
+      }));
+    }
+
     this.setState({
-      isOwner: plant && isOwner(plant, user),
+      isOwner: owner,
       plant
     });
 
