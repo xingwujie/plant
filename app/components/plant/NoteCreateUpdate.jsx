f5b6793316bfa4b9ac1f38ce65cb5b029f4315cf
Guy Ellis <wildfiction@gmail.com>
2016-01-19 20:39:20 -0700

    Add Cancel to Note Create

--- app/components/plant/NoteCreateUpdate.jsx
+++ app/components/plant/NoteCreateUpdate.jsx
@@ -1,6 +1,7 @@
 // Used to add a note to a plant
 
 import React from 'react';
+import * as actions from '../../actions';
 
 export default class NoteCreateUpdate extends React.Component {
 
@@ -14,28 +15,39 @@ export default class NoteCreateUpdate extends React.Component {
     e.stopPropagation();
   }
 
+  cancel(e) {
+    this.props.dispatch(actions.createNote({
+      _id: this.props.plant._id,
+      enable: false
+    }));
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
   render() {
     // const plantId = _.get(this, 'props.params.id', '');
 
-    const {
-      plant,
-    } = this.props || {};
+    // const {
+    //   plant,
+    // } = this.props || {};
 
     return (
       <div className='well'>
-        <h2>Add Note</h2>
-        <div>Plant: {plant.title}</div>
-        {plant.description && <div>Description: {plant.description}</div>}
-          <form className='editor'>
 
-            <div style={{textAlign: 'center'}}>
-              <button
-                type='button'
-                className='btn btn-primary btn-lg'
-                onClick={this.save.bind(this)}>Save Note</button>
-            </div>
+        <form className='editor'>
 
-          </form>
+          <div style={{textAlign: 'center'}}>
+            <button
+              type='button'
+              className='btn btn-primary btn-lg'
+              onClick={this.save.bind(this)}>Save</button>
+            <button
+              type='button'
+              className='btn btn-danger btn-lg'
+              onClick={this.cancel.bind(this)}>Cancel</button>
+          </div>
+
+        </form>
       </div>
     );
   }
