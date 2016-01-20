514acae488b88a4ddbfc1c8234ec81af2f14b1b3
Guy Ellis <wildfiction@gmail.com>
2016-01-20 08:43:37 -0700

    Add material-ui to project

--- app/main.js
+++ app/main.js
@@ -17,6 +17,13 @@ import Profile from './components/Profile';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
 // TODO: Put a Not Found / No Match component in here.
 var routes = (
   <Route path='/' component={App}>
