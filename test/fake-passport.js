18e45226bab49fd083148cfa92b076448afae018
Guy Ellis <wildfiction@gmail.com>
2016-02-02 05:51:34 -0700

    Hopefully fixes Travis test

--- test/fake-passport.js
+++ test/fake-passport.js
@@ -1,28 +1,33 @@
+import d from 'debug';
+const debug = d('plant:test.fake-passport');
 
 export default {
 
   initialize: () => {
-    console.log('initialize');
+    debug('fake fb initialize setup');
     return (req, res, next) => {
-      console.log('initialize called');
+      debug('fake fb initialize called');
       next();
     };
   },
 
   authenticate: (type, cb) => {
     if(cb) {
+      debug('fake fb authenticate called with cb');
       const err = null;
       const user = {}; // TODO: Finish this
       const info = {}; // What is this?
       return cb(err, user, info);
     } else {
+      debug('fake fb authenticate setup');
       return (req, res, next) => {
+        debug('fake fb authenticate called');
         return next();
       };
     }
   },
 
   use: (/* strategy */) => {
-    console.log('use:', arguments.length);
+    debug('fake fb use:', arguments.length);
   }
 };
--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,10 @@
 require('node-version-checker');
 process.env.PLANT_DB_NAME = 'plant-automated-testing';
 
+process.env.PLANT_FB_ID = '<fake-fb-id>';
+process.env.PLANT_FB_SECRET = '<fake-fb-secret>';
+process.env.PLANT_FB_CALLBACK_URL = '/auth/facebook/callback';
+
 if(!process.env.PLANT_DB_ACCOUNT) {
   console.log('No PLANT_DB_ACCOUNT ENV');
   process.exit(1);
