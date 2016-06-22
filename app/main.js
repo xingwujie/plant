e0b4de72f529533506a0a218e9599d52314ea240
Guy Ellis <wildfiction@gmail.com>
2016-06-22 15:57:01 -0700

    Fix material-ui changes to remove errors.

--- app/main.js
+++ app/main.js
@@ -4,11 +4,14 @@ import 'bootstrap.css';
 import './stylesheets/main.css';
 
 import {browserHistory, Router, Route, IndexRoute} from 'react-router';
+import {deepOrange500} from 'material-ui/styles/colors';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import App from './components/App';
 import Auth from './components/Auth';
 import DebugSettings from './components/DebugSettings';
 import Help from './components/Help';
 import Home from './components/Home';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import Login from './components/auth/Login';
 import Plant from './components/plant/Plant';
 import Plants from './components/plants/Plants';
@@ -16,12 +19,14 @@ import Privacy from './components/info/Privacy';
 import Profile from './components/Profile';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-//Needed for onTouchTap
-//Can go away when react 1.0 release
-//Check this repo:
-//https://github.com/zilverline/react-tap-event-plugin
-import injectTapEventPlugin from 'react-tap-event-plugin';
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
 injectTapEventPlugin();
 
 // TODO: Put a Not Found / No Match component in here.
@@ -47,7 +52,11 @@ function render() {
   document.body.appendChild(content);
   content = document.getElementById('content');
 
-  ReactDOM.render((<Router history={browserHistory}>{routes}</Router>), content);
+  ReactDOM.render((
+    <MuiThemeProvider muiTheme={muiTheme}>
+      <Router history={browserHistory}>{routes}</Router>
+    </MuiThemeProvider>
+  ), content);
 
 }
 
