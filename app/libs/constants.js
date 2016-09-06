8cc13807dda69e523d46d21584a90cec9341d48e
Guy Ellis <wildfiction@gmail.com>
2016-09-05 18:16:33 -0700

    Delete images from S3 when note is deleted. Fix bucket/key bug. Issue #381

--- app/libs/constants.js
+++ app/libs/constants.js
@@ -2,5 +2,6 @@ export default {
   uuidRE: /^[0-9a-f]{32}$/i,
   // Mongo _id regex: 57acfcf5ffaf0524572d58e8
   mongoIdRE: /^[0-9a-f]{24}$/i,
-  maxImageFilesPerUpload: 100
+  maxImageFilesPerUpload: 100,
+  awsBucketName: 'i.plaaant.com'
 };
