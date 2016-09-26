cdea57934b18b1e6f0853ffc4cf2b38f9a7da190
Guy Ellis <wildfiction@gmail.com>
2016-09-25 17:10:32 -0700

    Remove commented code. Issue #514

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -78,6 +78,16 @@ export default class Home extends React.Component {
     );
   }
 
+  anonHome() {
+    return (<div id='hero'>
+      <div className='home-header'>Trees and Plants</div>
+      <div className='home-subheader'>Increase your success through tracking</div>
+      <div className='home-subheader'>
+        <div><Link to={'/login'}>{'Login'}</Link>{' to get started'}</div>
+      </div>
+    </div>);
+  }
+
   renderUsers() {
     const {
       users = {},
@@ -88,54 +98,6 @@ export default class Home extends React.Component {
     } else {
       return this.anonHome();
     }
-  }
-
-  // renderUserPlants() {
-  //   const {
-  //     user = {},
-  //     userPlants: plants = {}
-  //   } = this.state;
-  //   if(!user.isLoggedIn) {
-  //     console.warn('user not logged in:', user);
-  //     return null;
-  //   }
-
-  //   if(isEmpty(plants)) {
-
-  //     return (
-  //       <div id='hero'>
-  //         <div className='home-header'>
-  //           {'Ready to add your first Plant or Tree?'}
-  //         </div>
-  //         <div className='home-subheader'>
-  //           <Link to='/plant'>{'Let\'s Do It'}</Link>
-  //         </div>
-  //       </div>
-  //     );
-
-  //   } else {
-  //     const numPlants = Object.keys(plants).length;
-  //     return (
-  //       <div id='hero'>
-  //         <div className='home-header'>
-  //           {`You have ${numPlants} plant${numPlants > 1 ? 's' : ''} in your collection. `}
-  //         </div>
-  //         <div className='home-subheader'>
-  //           <Link to={utils.makePlantsUrl(user)}>{'Go to plant collection...'}</Link>
-  //         </div>
-  //       </div>
-  //     );
-  //   }
-  // }
-
-  anonHome() {
-    return (<div id='hero'>
-      <div className='home-header'>Trees and Plants</div>
-      <div className='home-subheader'>Increase your success through tracking</div>
-      <div className='home-subheader'>
-        <div><Link to={'/login'}>{'Login'}</Link>{' to get started'}</div>
-      </div>
-    </div>);
   }
 
   render() {
