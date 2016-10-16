7c1fd73020c5eec3a76376710e7e6381b61bdc5d
Guy Ellis <wildfiction@gmail.com>
2016-10-16 16:08:24 -0700

    Enable Redux logging in prod to try and track down a prod only bug. Issue #577

--- app/store/index.js
+++ app/store/index.js
@@ -5,10 +5,13 @@ const {setupSubscribe: userSubscribe} = require('./user');
 
 let middleware = [api];
 
-if (process.env.NODE_ENV !== 'production') {
-  const logger = require('../middleware/logger');
-  middleware = [logger, api];
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   const logger = require('../middleware/logger');
+//   middleware = [logger, api];
+// }
+
+const logger = require('../middleware/logger');
+middleware = [logger, api];
 
 // Add the api to the pipeline/chain
 const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
