8de9576f875db96f0a387c05174428218f90ac67
Guy Ellis <wildfiction@gmail.com>
2016-12-16 20:41:00 -0700

    Fix redirecting of /plants/slug/user-id. Fixes #747

--- app/components/plants/Plants.jsx
+++ app/components/plants/Plants.jsx
@@ -1,5 +1,5 @@
 // Used to show a list of plants for a user.
-// Url: /plants/<optional-user-id>
+// Url: /plants/<slug>/<optional-user-id>
 // Now it redirects to a Location owned by the user
 // Redirect: /location/slug/_location_id
 
@@ -13,37 +13,58 @@ class Plants extends React.Component {
     router: React.PropTypes.object.isRequired
   };
 
+  constructor() {
+    super();
+    this.onChange = this.onChange.bind(this);
+    this.redirectIfReady = this.redirectIfReady.bind(this);
+
+    this.state = {};
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  onChange() {
+    const users = store.getState().get('users');
+    const locations = store.getState().get('locations');
+    this.setState({users, locations});
+  }
+
   componentWillMount() {
+    this.unsubscribe = store.subscribe(this.onChange);
+    this.onChange();
+    this.redirectIfReady();
+  }
+
+  componentWillUpdate() {
+    this.redirectIfReady();
+  }
+
+  redirectIfReady() {
     const userId = this.props.params && this.props.params.id;
     let fwdUrl = '/';
     if(userId) {
       const user = store.getState().getIn(['users', userId], Immutable.Map());
       const locationIds = user.get('locationIds', Immutable.List());
       if(locationIds.size) {
-        const locationId = locationIds.get(0);
+        const locationId = locationIds.first();
         const location = store.getState().getIn(['locations', locationId]);
         if(location) {
           const title = location.get('title', '');
           fwdUrl = `/location/${utils.makeSlug(title)}/${locationId}`;
-        } else {
-          console.warn('No location found', locationId);
+          this.context.router.push(fwdUrl);
         }
-      } else {
-        const slug = utils.makeSlug(user.get('name', 'unknown-gardener'));
-        fwdUrl = `/locations/${slug}/${userId}`;
-        // console.warn('No locationIds found', store.getState().getIn(['users', userId], Immutable.Map()).toJS());
       }
     } else {
       console.warn('No params.id', this.props);
     }
-
-    this.context.router.push(fwdUrl);
   }
 
   render() {
     return (
       <div>
-        {'Should have forwarded....'}
+        {'Working on redirecting you to the right place...'}
       </div>
     );
   }
