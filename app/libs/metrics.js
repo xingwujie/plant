c89341efb147702c5890240674e18835553e556f
Guy Ellis <wildfiction@gmail.com>
2017-05-20 15:04:38 -0700

    Refactor since-last-note out of reducer for notes list. Issue #1087

--- app/libs/metrics.js
+++ app/libs/metrics.js
@@ -2,6 +2,17 @@
 
 const utils = require('./utils');
 
+function since(lastNoteDate, note, noteId, acc) {
+  const currentNoteDate = utils.intToMoment(note.get('date'));
+  const sinceLast = lastNoteDate
+    ? `...and then after ${lastNoteDate.from(currentNoteDate, true)}`
+    : '';
+  if(sinceLast && !lastNoteDate.isSame(currentNoteDate)) {
+    acc.push({noteId, sinceLast, type:'since'});
+  }
+  return currentNoteDate;
+}
+
 /**
  *
  * @param {String[]} sortedNoteIds - an array of noteIds sorted by date
@@ -11,17 +22,17 @@ const utils = require('./utils');
  */
 function notesToMetricNotes(sortedNoteIds, notes) {
   let lastNoteDate;
+  // const metrics = {
+  //   lastHeight: 0,
+  //   lastHeightDate: 0,
+  //   lastGirth: 0,
+  //   lastGirthDate: 0,
+  //   lastExists: false,
+  // };
   return sortedNoteIds.reduce((acc, noteId) => {
     const note = notes.get(noteId);
     if(note) {
-      const currentNoteDate = utils.intToMoment(note.get('date'));
-      const sinceLast = lastNoteDate
-        ? `...and then after ${lastNoteDate.from(currentNoteDate, true)}`
-        : '';
-      if(sinceLast && !lastNoteDate.isSame(currentNoteDate)) {
-        acc.push({noteId, sinceLast, type:'since'});
-      }
-      lastNoteDate = currentNoteDate;
+      lastNoteDate = since(lastNoteDate, note, noteId, acc);
       acc.push({noteId, note, type: 'note'});
     } else {
       acc.push({noteId, type:'unfound'});
